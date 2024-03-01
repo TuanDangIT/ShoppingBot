07f32b8a09470345d478e02f2251eab707b8dc7c
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-03-01 16:27:35 +0100

    Add DI container

--- ShoppingBot/ShoppingBot/Program.cs
+++ ShoppingBot/ShoppingBot/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = CreateHostBuilder(args)
+    .Build();
+var serviceProvider = host.Services.CreateScope().ServiceProvider;
+IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration(app =>
+        {
+            app.AddJsonFile("appsettings.json", true, true);
+        })
+        .ConfigureServices((context, services) =>
+        {
+
+        });
+}
