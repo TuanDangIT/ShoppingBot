a7e8a3929fb0dfc0d95da97dc38533c3c25c84fb
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-03-05 22:31:41 +0100

    Add audit logging

--- ShoppingBot/ShoppingBot/Entities/Product.cs
+++ ShoppingBot/ShoppingBot/Entities/Product.cs
@@ -12,5 +12,7 @@ namespace ShoppingBot.Entities
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
         public double Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set;}
     }
 }
