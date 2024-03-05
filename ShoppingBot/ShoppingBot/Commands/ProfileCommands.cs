56ac363315c472e4da1cfd5fa281ada42f863902
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-03-05 22:12:29 +0100

    Add commands

---
+++ ShoppingBot/ShoppingBot/Commands/ProfileCommands.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBot.Commands
+{
+    internal class ProfileCommands : BaseCommandModule
+    {
+    }
+}
