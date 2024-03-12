711d26caccf6e1d29cada57eb002b4f8ec7a325b
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-03-12 23:25:35 +0100

    add product commands

--- ShoppingBot/ShoppingBot/Commands/ProductSlashCommands.cs
+++ ShoppingBot/ShoppingBot/Commands/ProductSlashCommands.cs
@@ -1,6 +1,9 @@
 ﻿using DSharpPlus.SlashCommands;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ShoppingBot.DAL;
 using ShoppingBot.Features.Product.AddProduct;
+using ShoppingBot.Features.Product.GetAllProducts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +16,18 @@ namespace ShoppingBot.Commands
     internal class ProductSlashCommands : ApplicationCommandModule
     {
         private readonly IMediator _mediator;
+        private readonly ShoppingBotDbContext _dbContext;
 
-        public ProductSlashCommands(IMediator mediator)
+        public ProductSlashCommands(IMediator mediator, ShoppingBotDbContext dbContext)
         {
             _mediator = mediator;
+            _dbContext = dbContext;
         }
         [SlashCommand("add-product", "add product")]
-        public async Task AddProduct(InteractionContext ctx)
+        public async Task AddProduct(InteractionContext ctx, [Option("n", "n")]string name,
+            [Option("p", "p")]double price, [Option("d", "d")] string description, [Option("i", "i")] string imageUrl)
         {
-            //var result = await _mediator.Send(new AddProductCommand(name, Double.Parse(price), description, imageUrl));
-            var result = await _mediator.Send(new AddProductCommand("1", 1, "1", "1"));
+            var result = await _mediator.Send(new AddProductCommand(name, price, description, imageUrl));
             if (result.IsFailure)
             {
                 await ctx.Interaction.CreateResponseAsync(DSharpPlus.InteractionResponseType.ChannelMessageWithSource,
@@ -33,6 +38,16 @@ namespace ShoppingBot.Commands
             await ctx.Interaction.CreateResponseAsync(DSharpPlus.InteractionResponseType.ChannelMessageWithSource,
                 new DSharpPlus.Entities.DiscordInteractionResponseBuilder()
                     .WithContent("Product successfully added!"));
+        }
+        [SlashCommand("get-first-product", "get first product")]
+        public async Task GetAllProducts(InteractionContext ctx)
+        {
+            var result = await _dbContext.Products.FirstOrDefaultAsync();
+            if (result is null)
+                return;
+            await ctx.Interaction.CreateResponseAsync(DSharpPlus.InteractionResponseType.ChannelMessageWithSource,
+                new DSharpPlus.Entities.DiscordInteractionResponseBuilder()
+                    .WithContent($"Product: {result.Name}, {result.Price}"));
         }
     }
 }
