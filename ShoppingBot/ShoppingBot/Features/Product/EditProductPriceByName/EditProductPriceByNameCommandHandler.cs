97d21d519969311859e6a756e4a5231d02d2aebd
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-03-17 22:13:02 +0100

    add validation to command

--- ShoppingBot/ShoppingBot/Features/Product/EditProductPriceByName/EditProductPriceByNameCommandHandler.cs
+++ ShoppingBot/ShoppingBot/Features/Product/EditProductPriceByName/EditProductPriceByNameCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using ShoppingBot.DAL.Repositories.Interfaces;
+﻿using FluentValidation;
+using ShoppingBot.DAL.Repositories.Interfaces;
 using ShoppingBot.Shared;
 using ShoppingBot.Shared.Abstractions;
 using System;
@@ -12,15 +13,23 @@ namespace ShoppingBot.Features.Product.EditProductPriceByName
     internal class EditProductPriceByNameCommandHandler : ICommandHandler<EditProductPriceByNameCommand>
     {
         private readonly IProductRepository _productRepository;
+        private readonly IValidator<EditProductPriceByNameCommand> _validator;
 
-        public EditProductPriceByNameCommandHandler(IProductRepository productRepository)
+        public EditProductPriceByNameCommandHandler(IProductRepository productRepository,
+            IValidator<EditProductPriceByNameCommand> validator)
         {
             _productRepository = productRepository;
+            _validator = validator;
         }
         public async Task<Result> Handle(EditProductPriceByNameCommand request, CancellationToken cancellationToken)
         {
-            await _productRepository.EditProductPriceByNameAsync(request.Name, request.Price);
-            return Result.Success();
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+            if(validationResult.IsValid)
+            {
+                await _productRepository.EditProductPriceByNameAsync(request.Name, request.Price);
+                return Result.Success();
+            }
+            return Result.Failure();
         }
     }
 }
