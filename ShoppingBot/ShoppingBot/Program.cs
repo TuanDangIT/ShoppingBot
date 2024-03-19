62dcc7048daf5b3375e14a4cf506787430862e0c
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-03-19 10:54:09 +0100

    add comment to exception handler

--- ShoppingBot/ShoppingBot/Program.cs
+++ ShoppingBot/ShoppingBot/Program.cs
@@ -12,6 +12,7 @@ AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
 void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
 {
+    //loggowanie tu będzie
     Console.WriteLine("An exception has been thrown: " + e.GetType());
 }
 
