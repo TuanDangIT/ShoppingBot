0a9bfe033c441486b833d45a34be5637601b80fb
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-03-19 11:11:45 +0100

    add errors

--- ShoppingBot/ShoppingBot/Shared/Errors/ProductErrors.cs
+++ ShoppingBot/ShoppingBot/Shared/Errors/ProductErrors.cs
@@ -10,5 +10,8 @@ namespace ShoppingBot.Shared.Errors
     {
         public static readonly Error ValidationError = new Error("product-validation-error", "Product validation failed");
         public static readonly Error NotFound = new Error("product-not-found", "Product not found");
+        public static readonly Error NotCreated = new Error("product-not-created", "Product not created");
+        public static readonly Error NotDeleted = new Error("product-not-deleted", "Product not deleted");
+        public static readonly Error NotUpdated = new Error("product-not-found", "Product not updated");
     }
 }
