aacd444e82ea85a210e3029dcac44a6baed026a6
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-03-19 10:52:40 +0100

    add global exception handler

--- ShoppingBot/ShoppingBot/Program.cs
+++ ShoppingBot/ShoppingBot/Program.cs
@@ -8,6 +8,13 @@ using IHost host = CreateHostBuilder(args)
     .Build();
 var serviceProvider = host.Services.CreateScope().ServiceProvider;
 var bot = serviceProvider.GetRequiredService<ShoppingBot.ShoppingBot>();
+AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+{
+    Console.WriteLine("An exception has been thrown: " + e.GetType());
+}
+
 bot.RunAsync(host).Wait();
 IHostBuilder CreateHostBuilder(string[] args)
 {
---
+++ ShoppingBot/ShoppingBot/Shared/Errors/GlobalErrors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBot.Shared.Errors
+{
+    internal static class GlobalErrors
+    {
+        public static readonly Error GlobalError = new Error("global-error", "Something went wrong");
+    }
+}
