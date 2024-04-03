738838ed20a212be9f2638c97ac8b554988c4979
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-03 16:44:45 +0200

    add crud methods to order entity

---
+++ ShoppingBot/ShoppingBot/DAL/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingBot.Entities;
+using ShoppingBot.Features.Order.GetAllOrders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBot.DAL.Repositories.Interfaces
+{
+    internal interface IOrderRepository
+    {
+        IEnumerable<Order> GetAllOrderAsync();
+        Order GetOrderByIdAsync(Guid id);
+        Task<int> DeleteOrderByIdAsync(Guid id);
+    }
+}
---
+++ ShoppingBot/ShoppingBot/DAL/Repositories/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using ShoppingBot.DAL.Repositories.Interfaces;
+using ShoppingBot.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBot.DAL.Repositories
+{
+    internal class OrderRepository : IOrderRepository
+    {
+        public Task<int> DeleteOrderByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> GetAllOrderAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrderByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
