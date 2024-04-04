86be1a2948f27434c297ec96a6cc4ce0b3cc079e
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-04 20:55:09 +0200

    create partial classes to orderslashcommands

--- ShoppingBot/ShoppingBot/Features/Order/GetAllOrders/GetAllOrdersSlashCommand.cs
+++ ShoppingBot/ShoppingBot/Features/Order/GetAllOrders/GetAllOrdersSlashCommand.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using DSharpPlus.SlashCommands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShoppingBot.Features.Order.GetAllOrders
+namespace ShoppingBot.Features.Order
 {
-    internal class GetAllOrdersSlashCommand
+    internal partial class OrderSlashCommand
     {
+        [SlashCommand("get-order-by-id", "Get a order by id")]
+        public async Task GetAllOrders(InteractionContext ctx,
+            [Option("id", "Order id")] Guid id)
+        {
+               
+        }
     }
 }
--- ShoppingBot/ShoppingBot/Features/Order/GetOrderById/GetOrderByIdSlashCommand.cs
+++ ShoppingBot/ShoppingBot/Features/Order/GetOrderById/GetOrderByIdSlashCommand.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using DSharpPlus.SlashCommands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShoppingBot.Features.Order.GetOrderById
+namespace ShoppingBot.Features.Order
 {
     internal partial class OrderSlashCommand
     {
+        [SlashCommand("get-order-by-id", "Get a order by id")]
+        public async Task GetOrderById(InteractionContext ctx,
+            [Option("id", "Order id")] Guid id)
+        {
 
+        }
     }
 }
