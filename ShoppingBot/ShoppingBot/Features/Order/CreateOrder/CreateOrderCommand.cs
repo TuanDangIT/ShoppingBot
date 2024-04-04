28f586a536ae82c4612963913835ae6538fe570f
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-04 21:12:23 +0200

    add properties to create order command

--- ShoppingBot/ShoppingBot/Features/Order/CreateOrder/CreateOrderCommand.cs
+++ ShoppingBot/ShoppingBot/Features/Order/CreateOrder/CreateOrderCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,5 @@ using System.Threading.Tasks;
 
 namespace ShoppingBot.Features.Order.CreateOrder
 {
-    internal class CreateOrderCommand
-    {
-    }
+    internal record class CreateOrderCommand(string Buyer, string ProductName, string ServerId) : IRequest;
 }
