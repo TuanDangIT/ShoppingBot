89cabf374a857c6fcef08b39c3dd82d6078b7d29
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-04 21:13:18 +0200

    change interface implementation

--- ShoppingBot/ShoppingBot/Features/Order/CreateOrder/CreateOrderCommand.cs
+++ ShoppingBot/ShoppingBot/Features/Order/CreateOrder/CreateOrderCommand.cs
@@ -1,11 +1,13 @@
 ﻿using MediatR;
+using ShoppingBot.Shared.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ShoppingBot.Features.Order.CreateOrder
 {
-    internal record class CreateOrderCommand(string Buyer, string ProductName, string ServerId) : IRequest;
+    internal record class CreateOrderCommand(string Buyer, string ProductName, string ServerId) : Shared.Abstractions.ICommand;
 }
