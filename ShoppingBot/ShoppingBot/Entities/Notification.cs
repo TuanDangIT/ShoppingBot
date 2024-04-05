8b5662c77d7c9ae387500d2822ff5e5a83e3a27c
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-05 12:33:14 +0200

    add notification entity

---
+++ ShoppingBot/ShoppingBot/Entities/Notification.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingBot.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBot.Entities
+{
+    internal class Notification : IAuditable
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Content { get; set; } = default!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
