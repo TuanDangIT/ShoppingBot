eca404e7294615db24be8131f1aabb9a2a0a765d
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-09 12:46:14 +0200

    create deletecommand record

--- ShoppingBot/ShoppingBot/Features/Order/DeleteOrderById/DeleteOrderByIdCommand.cs
+++ ShoppingBot/ShoppingBot/Features/Order/DeleteOrderById/DeleteOrderByIdCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShoppingBot.Shared.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,5 @@ using System.Threading.Tasks;
 
 namespace ShoppingBot.Features.Order.DeleteOrderById
 {
-    internal class DeleteOrderByIdCommand
-    {
-    }
+    internal record class DeleteOrderByIdCommand(Guid Id) : ICommand;
 }
