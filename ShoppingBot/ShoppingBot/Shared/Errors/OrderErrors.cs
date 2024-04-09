ce46f1696660c3896736bbf129fb007799eec972
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-09 13:12:34 +0200

    add validation error to orders

--- ShoppingBot/ShoppingBot/Shared/Errors/OrderErrors.cs
+++ ShoppingBot/ShoppingBot/Shared/Errors/OrderErrors.cs
@@ -8,6 +8,7 @@ namespace ShoppingBot.Shared.Errors
 {
     internal static class OrderErrors
     {
+        public static readonly Error ValidationError = new Error("order-validation-error", "Order validation failed");
         public static readonly Error NotCreated = new Error("order-not-created", "Order not created");
         public static readonly Error NotDeleted = new Error("order-not-deleted", "Order not deleted");
         public static readonly Error NotFound = new Error("order-not-found", "Order not found");
