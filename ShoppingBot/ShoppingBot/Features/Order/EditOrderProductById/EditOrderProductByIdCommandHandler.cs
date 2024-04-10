b0759d05e981c71bb62b2c02613c8ebb9b7ca0d7
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-10 19:40:39 +0200

    add editorder command handler

--- ShoppingBot/ShoppingBot/Features/Order/EditOrderProductById/EditOrderProductByIdCommandHandler.cs
+++ ShoppingBot/ShoppingBot/Features/Order/EditOrderProductById/EditOrderProductByIdCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using ShoppingBot.DAL.Repositories;
+using ShoppingBot.DTOs;
+using ShoppingBot.Shared;
+using ShoppingBot.Shared.Abstractions;
+using ShoppingBot.Shared.Errors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,35 @@ using System.Threading.Tasks;
 
 namespace ShoppingBot.Features.Order.EditOrderProductById
 {
-    internal class EditOrderProductByIdCommandHandler
+    internal class EditOrderProductByIdCommandHandler : ICommandHandler<EditOrderProductByIdCommand>
     {
+        private readonly ProductRepository _productRepository;
+        private readonly OrderRepository _orderRepository;
+
+        public EditOrderProductByIdCommandHandler(ProductRepository productRepository, OrderRepository orderRepository)
+        {
+            _productRepository = productRepository;
+            _orderRepository = orderRepository;
+        }
+        public async Task<Result> Handle(EditOrderProductByIdCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByNameAsync(request.Name, request.ServerId);
+            if(product is null)
+            {
+                return Result.Failure(ProductErrors.NotFound);
+            }
+            var order = await _orderRepository.GetOrderByIdAsync(request.Id, request.ServerId);
+            if (order is null)
+            {
+                return Result.Failure(OrderErrors.NotFound);
+            }
+            var change = await _orderRepository.EditOrderProductAsync(order, product);
+            if(change is 0)
+            {
+                return Result.Failure(OrderErrors.NotUpdated);
+            }
+            return Result.Success();
+
+        }
     }
 }
