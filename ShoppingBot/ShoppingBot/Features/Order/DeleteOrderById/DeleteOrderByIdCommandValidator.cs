b16c224ac6b7b0c4d8d8bfbdc3245bb349ae94e0
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-11 23:52:48 +0200

    add delete order command validator

--- ShoppingBot/ShoppingBot/Features/Order/DeleteOrderById/DeleteOrderByIdCommandValidator.cs
+++ ShoppingBot/ShoppingBot/Features/Order/DeleteOrderById/DeleteOrderByIdCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace ShoppingBot.Features.Order.DeleteOrderById
 {
-    internal class DeleteOrderByIdCommandValidator
+    internal class DeleteOrderByIdCommandValidator : AbstractValidator<DeleteOrderByIdCommand>
     {
+        public DeleteOrderByIdCommandValidator()
+        {
+            RuleFor(x => x.ServerId)
+                .NotEmpty()
+                .NotNull();
+        }
     }
 }
