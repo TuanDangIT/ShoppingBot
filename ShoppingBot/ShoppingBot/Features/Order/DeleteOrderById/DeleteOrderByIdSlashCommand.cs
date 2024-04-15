5a68d3eddb56adc15f30ecd71739507446b9293a
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-15 21:50:21 +0200

    change delete implementation to generic ones

--- ShoppingBot/ShoppingBot/Features/Order/DeleteOrderById/DeleteOrderByIdSlashCommand.cs
+++ ShoppingBot/ShoppingBot/Features/Order/DeleteOrderById/DeleteOrderByIdSlashCommand.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+using ShoppingBot.Entities;
 using ShoppingBot.Features.Order.DeleteOrderById;
 
 namespace ShoppingBot.Features.Order
@@ -12,34 +13,36 @@ namespace ShoppingBot.Features.Order
         public async Task DeleteOrderById(InteractionContext ctx,
             [Option("id", "Order id")] string id)
         {
-            await ctx.DeferAsync();
-            DiscordEmbedBuilder outputEmbed;
-            var serverId = ctx.Guild.Id;
-            var result = await _mediator.Send(new DeleteOrderByIdCommand(Guid.Parse(id), serverId.ToString()));
-            if (result.IsFailure)
-            {
-                outputEmbed = new DiscordEmbedBuilder
-                {
-                    Color = DiscordColor.Red,
-                    Title = $"Order operation response",
-                    Description = "Order delete operation failed",
-                    Footer = new DiscordEmbedBuilder.EmbedFooter()
-                    {
-                        Text = $"Additional information: \n" +
-                        $"{result.Error.Code}: {result.Error.Description}"
-                    }
-                };
-            }
-            else
-            {
-                outputEmbed = new DiscordEmbedBuilder
-                {
-                    Color = DiscordColor.Green,
-                    Title = $"Order operation response",
-                    Description = "Order delete operation successed"
-                };
-            }
-            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(outputEmbed));
+            Guid.TryParse(id, out var guidParsed);
+            await Delete<Entities.Order>(ctx, new DeleteOrderByIdCommand(guidParsed, ctx.Guild.Id.ToString()));
+            //await ctx.DeferAsync();
+            //DiscordEmbedBuilder outputEmbed;
+            //var serverId = ctx.Guild.Id;
+            //var result = await _mediator.Send(new DeleteOrderByIdCommand(Guid.Parse(id), serverId.ToString()));
+            //if (result.IsFailure)
+            //{
+            //    outputEmbed = new DiscordEmbedBuilder
+            //    {
+            //        Color = DiscordColor.Red,
+            //        Title = $"Order operation response",
+            //        Description = "Order delete operation failed",
+            //        Footer = new DiscordEmbedBuilder.EmbedFooter()
+            //        {
+            //            Text = $"Additional information: \n" +
+            //            $"{result.Error.Code}: {result.Error.Description}"
+            //        }
+            //    };
+            //}
+            //else
+            //{
+            //    outputEmbed = new DiscordEmbedBuilder
+            //    {
+            //        Color = DiscordColor.Green,
+            //        Title = $"Order operation response",
+            //        Description = "Order delete operation successed"
+            //    };
+            //}
+            //await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(outputEmbed));
         }
     }
 }
