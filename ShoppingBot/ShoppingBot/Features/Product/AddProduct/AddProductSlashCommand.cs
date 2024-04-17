4d51c8e149e34edc2b013f7ae61f4c01082b77dd
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-04-17 12:25:27 +0200

    use get generic method

--- ShoppingBot/ShoppingBot/Features/Product/AddProduct/AddProductSlashCommand.cs
+++ ShoppingBot/ShoppingBot/Features/Product/AddProduct/AddProductSlashCommand.cs
@@ -10,6 +10,7 @@ using ShoppingBot.Features.Product.AddProduct;
 using ShoppingBot.Shared;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.SlashCommands.Attributes;
+using ShoppingBot.Entities;
 namespace ShoppingBot.Features.Product
 {
     internal partial class ProductSlashCommands
@@ -19,41 +20,42 @@ namespace ShoppingBot.Features.Product
         public async Task AddProduct(InteractionContext ctx, [Option("name", "Item name")] string name,
             [Option("price", "Item price")] double price, [Option("description", "Item description")] string description, [Option("image-url", "Url link to the image")] string imageUrl)
         {
-            await ctx.DeferAsync();
-            var serverId = ctx.Guild.Id;
-            var result = await _mediator.Send(new AddProductCommand(name, price, description, imageUrl, serverId.ToString()));
-            var outputEmbed = new DiscordEmbedBuilder();
-            if (result.IsFailure)
-            {
-                IValidationResult validationResult = (IValidationResult)result;
-                StringBuilder resultsStringBuilder = new StringBuilder();
-                foreach(var error in validationResult.Errors)
-                {
-                    resultsStringBuilder.Append($"{error.Code}: {error.Description}\n");
-                }
-                outputEmbed = new DiscordEmbedBuilder
-                {
-                    Color = DiscordColor.Red,
-                    Title = $"Product operation response",
-                    Description = $"Product operation failed!",
-                    Footer = new DiscordEmbedBuilder.EmbedFooter()
-                    {
-                        Text = "Additional information:\n" +
-                        $"{resultsStringBuilder}"
-                    }
-                };
-            }
-            else
-            {
-                outputEmbed = new DiscordEmbedBuilder
-                {
-                    Color = DiscordColor.Green,
-                    Title = $"Product operation response",
-                    Description = $"Product operation successed!"
-                };
-            }
+            await Create<Entities.Product>(ctx, new AddProductCommand(name, price, description, imageUrl, ctx.Guild.Id.ToString()));
+            //await ctx.DeferAsync();
+            //var serverId = ctx.Guild.Id;
+            //var result = await _mediator.Send(new AddProductCommand(name, price, description, imageUrl, serverId.ToString()));
+            //var outputEmbed = new DiscordEmbedBuilder();
+            //if (result.IsFailure)
+            //{
+            //    IValidationResult validationResult = (IValidationResult)result;
+            //    StringBuilder resultsStringBuilder = new StringBuilder();
+            //    foreach(var error in validationResult.Errors)
+            //    {
+            //        resultsStringBuilder.Append($"{error.Code}: {error.Description}\n");
+            //    }
+            //    outputEmbed = new DiscordEmbedBuilder
+            //    {
+            //        Color = DiscordColor.Red,
+            //        Title = $"Product operation response",
+            //        Description = $"Product operation failed!",
+            //        Footer = new DiscordEmbedBuilder.EmbedFooter()
+            //        {
+            //            Text = "Additional information:\n" +
+            //            $"{resultsStringBuilder}"
+            //        }
+            //    };
+            //}
+            //else
+            //{
+            //    outputEmbed = new DiscordEmbedBuilder
+            //    {
+            //        Color = DiscordColor.Green,
+            //        Title = $"Product operation response",
+            //        Description = $"Product operation successed!"
+            //    };
+            //}
 
-            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(outputEmbed));
+            //await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(outputEmbed));
         }
     }
 }
