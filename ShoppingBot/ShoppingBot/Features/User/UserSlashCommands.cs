ed7b0d369d2b6b26bf485fbe8a5b201e8320d360
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-05-01 23:02:14 +0200

    create template slash commands

---
+++ ShoppingBot/ShoppingBot/Features/User/UserSlashCommands.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus.SlashCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBot.Features.User
+{
+    [SlashCommandGroup("user", "User operations")]
+    internal partial class UserSlashCommands : ApplicationCommandModule
+    {
+    }
+}
