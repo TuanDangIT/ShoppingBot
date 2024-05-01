998a28501ab5827c3dac42c0fa15554a79e1ff28
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-05-01 23:06:31 +0200

    create register discord user command file

---
+++ ShoppingBot/ShoppingBot/Features/User/CreateUser/CreateUserSlashCommand.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.SlashCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBot.Features.User
+{
+    internal partial class UserSlashCommands
+    {
+        [SlashCommand("register-user", "Register yourself to conduct any operations")]
+        public async Task CreateUser(InteractionContext ctx)
+        {
+        }
+    }
+}
