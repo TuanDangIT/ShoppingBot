0919d408229fe5007f3a3fef890baf2f6ea8ddb5
Anh Tuan Dang <anh_tuan_dang@outlook.com>
2024-05-01 23:04:38 +0200

    create getuser command file

---
+++ ShoppingBot/ShoppingBot/Features/User/GetUser/GetUserSlashCommand.cs
@@ -0,0 +1,18 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBot.Features.User
+{
+    internal partial class UserSlashCommands
+    {
+        [SlashCommand("get-user", "Get user information")]
+        public async Task GetUser(InteractionContext ctx)
+        {
+        }
+    }
+}
